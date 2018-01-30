ff9428191b145c69656c4f4090bce221cb922fba
Harish Manana <harishmanana@gmail.com>
2018-01-30 16:59:47 +0530

    release

--- src/calcpack/CoreCalc.java
+++ src/calcpack/CoreCalc.java
@@ -2,14 +2,14 @@ package calcpack;
 
 public class CoreCalc {
 	public int add(int a, int b) {
-		return a+b;
-	}
-	
-	public int subtract(int a, int b) {
-		return a-b;
+		System.out.println("this is new line");
+		int result = a + b;
+		return result;
 	}
 	
 	public int multiply(int a, int b) {
+		System.out.println("MULTIPLE....");
+		
 		return a*b;
 	}
 	
